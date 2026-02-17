8afb0cdc07f9f471c25c7568587c69e5d5480a3a
Jordan Lundy <jordanlundy427@gmail.com>
2026-02-17 11:25:10 -0700

    configured the backend path in vite config file, started front end page and CSS

--- WorkOrderTracker.Web/src/App.jsx
+++ WorkOrderTracker.Web/src/App.jsx
@@ -1,8 +1,39 @@
+import "./App.css";
+
+import { useEffect, useState } from "react";
+
 export default function App() {
+  // this is where we will store orders
+  const [workorders, setOrders] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    async function loadOrders() {
+      const resp = await fetch("/api/workorders");
+      const data = await resp.json();
+      setOrders(data.items);
+      setTotal(data.total);
+    }
+
+    loadOrders();
+  }, []);
+
   return (
-    <div style={{ padding: 24 }}>
-      <h1>WorkOrderTracker</h1>
-      <p>Frontend starting point.</p>
+    <div>
+      <h1>Work Orders Tracker</h1>
+      <h2>Total orders:</h2>
+      <h3>{total}</h3>
+      <ul id="orders">
+        {workorders.map((o) => (
+          <li key={o.id}>
+            {o.title}
+            <br />
+            Status: {o.status}
+            <br />
+            Description: {o.description}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
